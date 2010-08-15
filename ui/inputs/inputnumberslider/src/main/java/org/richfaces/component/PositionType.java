d3755df2f5c67d60737e03f76df0d072ec5642de
Nick Belaevski <nbelaevski@exadel.com>
2010-08-15 18:42:51 +0000

    InputNumberSlider refactoring

---
+++ ui/inputs/inputnumberslider/src/main/java/org/richfaces/component/PositionType.java
@@ -0,0 +1,7 @@
+package org.richfaces.component;
+
+public enum PositionType {
+    top, right, bottom, left;
+    
+    public static final PositionType DEFAULT = right;
+}
--- ui/inputs/inputnumberslider/src/main/java/org/richfaces/renderkit/html/PositionType.java
+++
@@ -1,5 +0,0 @@
-package org.richfaces.renderkit.html;
-
-public enum PositionType {
-    top, right, bottom, left
-}
