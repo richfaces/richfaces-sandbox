cf5c956c467e6c69e97926fe35d68e8667b1e769
Anton Belevich <abelevich@exadel.com>
2010-11-18 14:26:51 +0000

    initial commit

---
+++ ui/drag-drop/ui/src/main/java/org/richfaces/component/AbstractDropSupport.java
@@ -0,0 +1,46 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright ${year}, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.richfaces.component;
+
+import org.richfaces.cdk.annotations.JsfComponent;
+import org.richfaces.cdk.annotations.JsfRenderer;
+import org.richfaces.cdk.annotations.Tag;
+
+/**
+ * @author abelevich
+ *
+ */
+@JsfComponent(
+        generate = "org.richfaces.component.UIDropSupport",
+        renderer = @JsfRenderer(type = "org.richfaces.DropSupportRenderer"),
+        tag = @Tag(name="dropSupport"),
+        type = AbstractDropSupport.COMPONENT_TYPE,
+        family = AbstractDropSupport.COMPONENT_FAMILY
+    )
+public abstract class AbstractDropSupport extends AbstractActionComponent {
+    
+    public static final String COMPONENT_TYPE = "org.richfaces.DropSupport";
+    
+    public static final String COMPONENT_FAMILY = "org.richfaces.DropSupport";
+
+}
---
+++ ui/drag-drop/ui/src/main/java/org/richfaces/renderkit/DragBehaviorRendererBase.java
@@ -0,0 +1,42 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright ${year}, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.richfaces.renderkit;
+
+import javax.faces.application.ResourceDependencies;
+import javax.faces.application.ResourceDependency;
+
+/**
+ * @author abelevich
+ *
+ */
+
+@ResourceDependencies({
+    @ResourceDependency(name = "jquery.js"),
+    @ResourceDependency(name = "jquery-ui-core.js"),
+    @ResourceDependency(name = "jquery-dnd.js"),
+    @ResourceDependency(name = "richfaces.js"), 
+    @ResourceDependency(name = "richfaces-dnd.js")
+})
+public class DragBehaviorRendererBase extends AjaxComponentRendererBase {
+    
+}
