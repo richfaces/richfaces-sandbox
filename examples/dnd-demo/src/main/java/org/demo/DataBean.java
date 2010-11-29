d3e66ca189769c457e5892ad2c533781cee6a056
Anton Belevich <abelevich@exadel.com>
2010-11-29 18:38:41 +0000

    extend demo

--- examples/dnd-demo/src/main/java/org/demo/DataBean.java
+++ examples/dnd-demo/src/main/java/org/demo/DataBean.java
@@ -1,5 +1,8 @@
 package org.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -9,7 +12,52 @@ import org.richfaces.event.DropEvent;
 @SessionScoped
 public class DataBean {
     
+    private List<String> dropValues = new ArrayList<String>();
+    
+    private String dragValue1 = "dragValue 1";
+    
+    private String dragValue2 = "dragValue 3";
+    
+    private String dragValue3 = "dragValue 3";
+    
+
+    public List<String> getDropValues(){
+        return dropValues;
+    }
+    
+    public String getDragValue1() {
+        return dragValue1;
+    }
+
+    public void setDragValue1(String dragValue1) {
+        this.dragValue1 = dragValue1;
+    }
+
+    public String getDragValue2() {
+        return dragValue2;
+    }
+
+    public void setDragValue2(String dragValue2) {
+        this.dragValue2 = dragValue2;
+    }
+
+    public String getDragValue3() {
+        return dragValue3;
+    }
+
+    public void setDragValue3(String dragValue3) {
+        this.dragValue3 = dragValue3;
+    }
+
+    public void setDropValues(List<String> dropValues){
+        this.dropValues = dropValues;
+    }
+    
     public void processEvent(DropEvent event) {
+        String value = (String)event.getDragValue();
+        dropValues.add(value);
         System.out.println("DataBean.processEvent()");
     }
+    
+    
 }
