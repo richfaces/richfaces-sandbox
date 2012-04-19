69aa74644f21ac58958ae16b88c46bc3e3dd4a4d
Brian Leathem <bleathem@gmail.com>
2012-04-19 09:41:34 -0700

    bootstrap: inial copy of component-template

---
+++ bootstrap/ui/src/main/java/org/richfaces/component/package-info.java
@@ -0,0 +1,6 @@
+@TagLibrary(shortName = "componentName", prefix = "componentName", uri = "http://richfaces.org/sandbox/componentName",
+        displayName = "ComponentName component tags")
+package org.richfaces.component;
+
+import org.richfaces.cdk.annotations.TagLibrary;
+
