927b3edab3293c3ff67e38723c21cc3fae6070e9
Brian Leathem <bleathem@gmail.com>
2012-04-19 23:35:02 -0700

    bootstrap: added the buttonGroup component

---
+++ bootstrap/ui/src/main/java/org/richfaces/bootstrap/component/AbstractButtonGroup.java
@@ -0,0 +1,52 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright , Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.richfaces.bootstrap.component;
+
+import org.richfaces.bootstrap.renderkit.ButtonGroupRendererBase;
+import org.richfaces.cdk.annotations.Attribute;
+import org.richfaces.cdk.annotations.JsfComponent;
+import org.richfaces.cdk.annotations.JsfRenderer;
+import org.richfaces.cdk.annotations.Tag;
+
+import javax.faces.component.UIPanel;
+
+/**
+ * Base class for the tab component
+ *
+ * @author <a href="http://community.jboss.org/people/bleathem">Brian Leathem</a>
+ */
+@JsfComponent(
+        type = AbstractButtonGroup.COMPONENT_TYPE,
+        family = AbstractButtonGroup.COMPONENT_FAMILY,
+        renderer = @JsfRenderer(type = ButtonGroupRendererBase.RENDERER_TYPE),
+        tag = @Tag(name="buttonGroup"))
+abstract public class AbstractButtonGroup extends UIPanel {
+    public static final String COMPONENT_FAMILY = "org.richfaces.bootstrap.ButtonGroup";
+    public static final String COMPONENT_TYPE = "org.richfaces.bootstrap.ButtonGroup";
+
+    @Attribute
+    public abstract String getTitle();
+
+    @Attribute
+    abstract public String getButtonStyle();
+
+}
---
+++ bootstrap/ui/src/main/java/org/richfaces/bootstrap/renderkit/ButtonGroupRendererBase.java
@@ -0,0 +1,49 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright , Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.richfaces.bootstrap.renderkit;
+
+import org.richfaces.bootstrap.component.AbstractButtonGroup;
+import org.richfaces.component.AbstractCommandLink;
+import org.richfaces.renderkit.html.DivPanelRenderer;
+
+import javax.faces.application.ResourceDependencies;
+import javax.faces.application.ResourceDependency;
+import javax.faces.component.UIComponent;
+
+@ResourceDependencies({
+        @ResourceDependency(library = "org.richfaces", name = "ajax.reslib"),
+        @ResourceDependency(library = "org.richfaces", name = "base-component.reslib"),
+        @ResourceDependency(library = "bootstrap/css", name = "bootstrap.css"),
+        @ResourceDependency(library = "bootstrap/js", name = "bootstrap.js")})
+/**
+ * Base class for the tab renderer
+ *
+ * @author <a href="http://community.jboss.org/people/bleathem">Brian Leathem</a>
+ */
+public abstract class ButtonGroupRendererBase extends DivPanelRenderer {
+    public static final String RENDERER_TYPE = "org.richfaces.bootstrap.ButtonGroupRenderer";
+
+    // A workaround for RF-11668
+    public AbstractButtonGroup castComponent(UIComponent component) {
+        return (AbstractButtonGroup) component;
+    }
+}
