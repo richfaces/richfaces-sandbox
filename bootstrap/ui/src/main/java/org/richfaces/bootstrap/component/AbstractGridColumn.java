2fe7e702bf1cccfce7a36daf977320c0b1670096
Brian Leathem <bleathem@gmail.com>
2012-04-24 20:22:46 -0700

    bootstrap: Added the gridRow and gridColumn components

---
+++ bootstrap/ui/src/main/java/org/richfaces/bootstrap/component/AbstractGridColumn.java
@@ -0,0 +1,60 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright , Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.richfaces.bootstrap.component;
+
+import org.richfaces.bootstrap.renderkit.GridColumnRendererBase;
+import org.richfaces.cdk.annotations.Attribute;
+import org.richfaces.cdk.annotations.JsfComponent;
+import org.richfaces.cdk.annotations.JsfRenderer;
+import org.richfaces.cdk.annotations.Tag;
+
+import javax.faces.component.UIPanel;
+
+/**
+ * Base class for the gridColumn component
+ *
+ * @author <a href="http://community.jboss.org/people/bleathem">Brian Leathem</a>
+ */
+@JsfComponent(
+        type = AbstractGridColumn.COMPONENT_TYPE,
+        family = AbstractGridColumn.COMPONENT_FAMILY,
+        renderer = @JsfRenderer(type = GridColumnRendererBase.RENDERER_TYPE),
+        tag = @Tag(name="gridColumn"))
+abstract public class AbstractGridColumn extends UIPanel {
+    public static final String COMPONENT_FAMILY = "org.richfaces.bootstrap.GridColumn";
+    public static final String COMPONENT_TYPE = "org.richfaces.bootstrap.GridColumn";
+
+    @Attribute(defaultValue = "1")
+    abstract public Integer getSpan();
+
+    @Attribute
+    abstract public Integer getOffset();
+
+    public String getSpanClass() {
+        return getSpan() == null ? "" : String.format("span%d", getSpan());
+    }
+
+    public String getOffsetClass() {
+        return getOffset() == null ? "" : String.format("offset%d", getOffset());
+    }
+
+}
---
+++ bootstrap/ui/src/main/java/org/richfaces/bootstrap/component/AbstractGridRow.java
@@ -0,0 +1,45 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright , Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.richfaces.bootstrap.component;
+
+import org.richfaces.bootstrap.renderkit.GridRowRendererBase;
+import org.richfaces.cdk.annotations.JsfComponent;
+import org.richfaces.cdk.annotations.JsfRenderer;
+import org.richfaces.cdk.annotations.Tag;
+
+import javax.faces.component.UIPanel;
+
+/**
+ * Base class for the gridRow component
+ *
+ * @author <a href="http://community.jboss.org/people/bleathem">Brian Leathem</a>
+ */
+@JsfComponent(
+        type = AbstractGridRow.COMPONENT_TYPE,
+        family = AbstractGridRow.COMPONENT_FAMILY,
+        renderer = @JsfRenderer(type = GridRowRendererBase.RENDERER_TYPE),
+        tag = @Tag(name="gridRow"))
+abstract public class AbstractGridRow extends UIPanel {
+    public static final String COMPONENT_FAMILY = "org.richfaces.bootstrap.GridRow";
+    public static final String COMPONENT_TYPE = "org.richfaces.bootstrap.GridRow";
+
+}
