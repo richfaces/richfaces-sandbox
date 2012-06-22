ad264057c1c205dd2b3a90beb44867b11ab5fb12
Paul Dijou <paul.dijou.dev@gmail.com>
2012-06-22 16:16:32 +0200

    [Bootstrap] 1st draft of modal component.

---
+++ bootstrap/ui/src/main/java/org/richfaces/bootstrap/component/AbstractModal.java
@@ -0,0 +1,79 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright , Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.richfaces.bootstrap.component;
+
+import javax.faces.component.UIPanel;
+
+import org.richfaces.bootstrap.renderkit.ModalRendererBase;
+import org.richfaces.cdk.annotations.Attribute;
+import org.richfaces.cdk.annotations.JsfComponent;
+import org.richfaces.cdk.annotations.JsfRenderer;
+import org.richfaces.cdk.annotations.Tag;
+import org.richfaces.component.attribute.CoreProps;
+
+/**
+ * Base class for the modal component
+ * 
+ * @author <a href="http://www.pauldijou.fr">Paul Dijou</a>
+ * 
+ */
+@JsfComponent(
+        type = AbstractModal.COMPONENT_TYPE,
+        family = AbstractModal.COMPONENT_FAMILY,
+        renderer = @JsfRenderer(type = ModalRendererBase.RENDERER_TYPE),
+        tag = @Tag(name="modal"))
+public abstract class AbstractModal extends UIPanel implements CoreProps {
+    public static final String COMPONENT_FAMILY = "org.richfaces.bootstrap.Modal";
+    public static final String COMPONENT_TYPE = "org.richfaces.bootstrap.Modal";
+    
+    @Attribute
+    public abstract String getHeader();
+    
+    @Attribute(defaultValue = "true")
+    public abstract boolean isClosable();
+    
+    @Attribute(defaultValue = "false")
+    public abstract boolean isCustom();
+    
+    @Attribute(defaultValue = "", suggestedValue = "fade")
+    public abstract String getEffect();
+    
+    @Attribute
+    public abstract HorizontalPosition getCancelPosition();
+    
+    public boolean hasLeftCancelButton() {
+        return HorizontalPosition.left.equals(getCancelPosition());
+    }
+    
+    public boolean hasRightCancelButton() {
+        return HorizontalPosition.right.equals(getCancelPosition());
+    }
+    
+    public boolean hasFacet(String facetName) {
+        return getFacet(facetName) != null && getFacet(facetName).isRendered();
+    }
+
+    public enum Facets {
+        header,
+        footer
+    }
+}
---
+++ bootstrap/ui/src/main/java/org/richfaces/bootstrap/component/AbstractModalBody.java
@@ -0,0 +1,46 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright , Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.richfaces.bootstrap.component;
+
+import javax.faces.component.UIPanel;
+import org.richfaces.bootstrap.renderkit.ModalBodyRendererBase;
+import org.richfaces.cdk.annotations.JsfComponent;
+import org.richfaces.cdk.annotations.JsfRenderer;
+import org.richfaces.cdk.annotations.Tag;
+import org.richfaces.component.attribute.CoreProps;
+
+/**
+ * Base class for the modalBody component
+ * 
+ * @author <a href="http://www.pauldijou.fr">Paul Dijou</a>
+ * 
+ */
+@JsfComponent(
+        type = AbstractModalBody.COMPONENT_TYPE,
+        family = AbstractModalBody.COMPONENT_FAMILY,
+        renderer = @JsfRenderer(type = ModalBodyRendererBase.RENDERER_TYPE),
+        tag = @Tag(name="modalBody"))
+public abstract class AbstractModalBody extends UIPanel implements CoreProps {
+    public static final String COMPONENT_FAMILY = "org.richfaces.bootstrap.ModalBody";
+    public static final String COMPONENT_TYPE = "org.richfaces.bootstrap.ModalBody";
+    
+}
