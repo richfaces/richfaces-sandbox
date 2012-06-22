03b44745d0044167eff177ae2e9040e830799678
Paul Dijou <paul.dijou.dev@gmail.com>
2012-06-22 18:22:21 +0200

    Billing+Invoice+little correction

--- bootstrap/demo/src/main/java/org/richfaces/bootstrap/SessionBean.java
+++ bootstrap/demo/src/main/java/org/richfaces/bootstrap/SessionBean.java
@@ -1,6 +1,8 @@
 package org.richfaces.bootstrap;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -18,4 +20,33 @@ public class SessionBean implements Serializable {
     public void setFluid(boolean fluid) {
         this.fluid = fluid;
     }
+    
+    private List<String> selectedValues;
+    private List<String> availableValues;
+
+    public List<String> getSelectedValues() {
+        return selectedValues;
+    }
+
+    public void setSelectedValues(List<String> selectedValues) {
+        this.selectedValues = selectedValues;
+    }
+
+    public List<String> getAvailableValues() {
+        if(availableValues == null) {
+            availableValues = new ArrayList<String>();
+            availableValues.add("Value1");
+            availableValues.add("Value2");
+            availableValues.add("Value3");
+            availableValues.add("Value4");
+        }
+        
+        return availableValues;
+    }
+
+    public void setAvailableValues(List<String> availableValues) {
+        this.availableValues = availableValues;
+    }
+    
+    
 }
