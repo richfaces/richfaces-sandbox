1c658a85fce3687c9654368d4ef8bc00187c7eba
Lukas Fryc <lfryc@redhat.com>
2012-08-10 19:42:25 +0200

    refactored multicomplete

--- bootstrap/ui/src/main/resources/META-INF/resources/org.richfaces/rf.autocomplete.js
+++ bootstrap/ui/src/main/resources/META-INF/resources/org.richfaces/rf.autocomplete.js
@@ -1,109 +1,54 @@
 (function($) {
-    $.widget("rf.autocomplete", $.extend({}, $.ui.autocomplete.prototype, {
+    $.widget("rf.multicomplete", $.extend({}, $.ui.autocomplete.prototype, {
         
         options: {
             suggestions: [],
-            uifocus: null,
-            uiselect: null,
-            multiselect: false,
             tokens: ",",
             source: function(request, response) {
-                this._delegate()._handleSource(request, response);
+                var searchTerm = this._extractLastToken(request.term);
+                response( this.options.suggest.call(this, searchTerm) );
+            },
+            suggest: function(searchTerm) {
+                return $.ui.autocomplete.filter( this.options.suggestions, searchTerm );
             }
         },
     
         _create : function() {
-            this.options.focus = this._delegate()._handleFocus;
-            this.options.select = this._delegate()._handleSelect;
-            
             // extend jQuery UI Autocomplete
             $.ui.autocomplete.prototype._create.apply(this, arguments);
-        },
-        
-        _setOption: function(key, value) {
+            
             var self = this;
-            if (this.options.key === value) {
-                return;
-            }
-            switch (key) {
-                case 'source':
-                    // disallow to rewrite 'source'
-                    if (this.options.source) {
-                        this._setOption('suggestions', value);
-                        return;
-                    }
-                    break;
-                case 'focus':
-                    // disallow to rewrite 'focus'
-                    if (this.options.focus) {
-                        this._setOption('uifocus', value);
-                        return;
-                    }
-                    break;
-                case 'select':
-                    // disallow to rewrite 'select'
-                    if (this.options.select) {
-                        this._setOption('uiselect', value);
-                        return;
-                    }
-                    break;
-            }
-            $.ui.autocomplete.prototype._setOption.apply(this, arguments);
+            
+            $(this.element).on("autocompletefocus", function( event, ui ) {
+                // prevent focus
+                return false;
+            });
+            
+            $(this.element).on("autocompleteselect", function( event, ui ) {
+                var terms = self._tokenize( this.value );
+                // remove the current input
+                terms.pop();
+                // add the selected item
+                terms.push( ui.item.value );
+                // add placeholder to get the comma-and-space at the end
+                terms.push( "" );
+                var firstToken = self.options.tokens.charAt(0);
+                this.value = terms.join( firstToken == " " ? " " : firstToken + " " );
+                return false;
+            });
         },
         
         _tokenize: function(term) {
-            if (this.options.tokens) {
-                return term.split( new RegExp("\\s*[" + this.options.tokens + "]\\s*") );
-            }
-            return [term];
+            return term.split( new RegExp("\\s*[" + this.options.tokens + "]\\s*") );
         },
         
         _extractLastToken: function(term) {
             return this._tokenize(term).pop();
-        },
-        
-        _delegate: function() {
-            var autocomplete = this;
-            return {
-                _handleSource: function(request, response) {
-                    var searchTerm;
-                    if (autocomplete.options.multiselect) {
-                        searchTerm = autocomplete._extractLastToken(request.term);
-                    } else {
-                        searchTerm = request.term;
-                    }
-                    response( $.ui.autocomplete.filter(
-                        autocomplete.options.suggestions, searchTerm ) );
-                },
-                
-                _handleFocus: function( event, ui ) {
-                    if (autocomplete.options.multiselect) {
-                        // prevent focus
-                        return false;
-                    }
-                },
-                
-                _handleSelect: function( event, ui ) {
-                    if (autocomplete.options.multiselect) {
-                        var terms = autocomplete._tokenize( this.value );
-                        // remove the current input
-                        terms.pop();
-                        // add the selected item
-                        terms.push( ui.item.value );
-                        // add placeholder to get the comma-and-space at the end
-                        terms.push( "" );
-                        var firstToken = autocomplete.options.tokens.charAt(0);
-                        this.value = terms.join( firstToken == " " ? " " : firstToken + " " );
-                        
-                        return false;
-                    }
-                }
-            };
         }
         
     }));
 
-    $.rf.autocomplete.prototype.options = $.extend({}, $.ui.autocomplete.prototype.options, $.rf.autocomplete.prototype.options);
+    $.rf.multicomplete.prototype.options = $.extend({}, $.ui.autocomplete.prototype.options, $.rf.multicomplete.prototype.options);
     
     
 })(jQuery);
