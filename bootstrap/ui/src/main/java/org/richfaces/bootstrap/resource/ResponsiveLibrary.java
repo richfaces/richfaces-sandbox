e74b70598c41421a8777c398b21a8cbf4d4fce0a
Lukas Fryc <lfryc@redhat.com>
2012-09-04 18:28:41 +0200

    compile responsive LESS resources only in production, otherwise, use development versions

--- bootstrap/ui/src/main/java/org/richfaces/bootstrap/resource/ResponsiveLibrary.java
+++ bootstrap/ui/src/main/java/org/richfaces/bootstrap/resource/ResponsiveLibrary.java
@@ -1,5 +1,8 @@
 package org.richfaces.bootstrap.resource;
 
+import javax.faces.application.ProjectStage;
+import javax.faces.context.FacesContext;
+
 import org.richfaces.resource.ResourceKey;
 import org.richfaces.resource.ResourceLibrary;
 
@@ -7,13 +10,24 @@ import com.google.common.collect.ImmutableList;
 
 public class ResponsiveLibrary implements ResourceLibrary {
 
-    private static final ImmutableList<ResourceKey> BOOTSTRAP_RESPONSIVE_MIN = ImmutableList.of(
-            ResourceKey.create("min/rf-bootstrap-responsive.css", "org.richfaces")
-        );
+    private static final ImmutableList<ResourceKey> BOOTSTRAP_RESPONSIVE_MIN = ImmutableList.of(ResourceKey.create("min/rf-bootstrap-responsive.css",
+            "org.richfaces"));
+
+    private static final ImmutableList<ResourceKey> BOOTSTRAP_RESPONSIVE_CLIENT_SIDE = ImmutableList.of(
+            ResourceKey.create("bootstrap-responsive.less", "org.richfaces.develop"),
+            ResourceKey.create("less.min.js", "less"),
+            ResourceKey.create("less.instant.js", "less"));
 
     @Override
     public Iterable<ResourceKey> getResources() {
-        return BOOTSTRAP_RESPONSIVE_MIN;
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ProjectStage stage = facesContext.getApplication().getProjectStage();
+
+        if (ProjectStage.Development == stage) {
+            return BOOTSTRAP_RESPONSIVE_CLIENT_SIDE;
+        } else {
+            return BOOTSTRAP_RESPONSIVE_MIN;
+        }
     }
 
 }
