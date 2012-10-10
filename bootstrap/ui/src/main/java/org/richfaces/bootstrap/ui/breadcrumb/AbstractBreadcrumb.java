f1e1c48475c86951768d914348f8471bcdd5d4e3
Lukas Fryc <lfryc@redhat.com>
2012-10-10 10:11:13 +0200

    RFSBOX-18: breadcrumb - added divider attribute

--- bootstrap/ui/src/main/java/org/richfaces/bootstrap/ui/breadcrumb/AbstractBreadcrumb.java
+++ bootstrap/ui/src/main/java/org/richfaces/bootstrap/ui/breadcrumb/AbstractBreadcrumb.java
@@ -1,31 +1,26 @@
 package org.richfaces.bootstrap.ui.breadcrumb;
 
-import javax.faces.component.UIComponent;
 import javax.faces.component.UIOutput;
-import javax.faces.component.UIPanel;
-import javax.faces.context.FacesContext;
-import javax.faces.context.ResponseWriter;
 
 import org.richfaces.bootstrap.javascript.BootstrapJSPlugin;
-import org.richfaces.bootstrap.semantic.AbstractMenuFacet;
-import org.richfaces.bootstrap.semantic.AbstractPositionFacet;
-import org.richfaces.bootstrap.ui.modal.AbstractModal;
-import org.richfaces.bootstrap.ui.modal.ModalRendererBase;
-import org.richfaces.bootstrap.ui.navbar.AbstractNavbar;
+import org.richfaces.cdk.annotations.Attribute;
 import org.richfaces.cdk.annotations.JsfComponent;
 import org.richfaces.cdk.annotations.JsfRenderer;
 import org.richfaces.cdk.annotations.Tag;
 
-import java.io.IOException;
-
 @BootstrapJSPlugin(name = "breadcrumb")
-@JsfComponent(type = AbstractBreadcrumb.COMPONENT_TYPE, family = AbstractBreadcrumb.COMPONENT_FAMILY, renderer = @JsfRenderer(type = BreadcrumbRendererBase.RENDERER_TYPE),
-        tag = @Tag(name = "breadcrumb"))
+@JsfComponent(type = AbstractBreadcrumb.COMPONENT_TYPE, family = AbstractBreadcrumb.COMPONENT_FAMILY, renderer = @JsfRenderer(type = BreadcrumbRendererBase.RENDERER_TYPE), tag = @Tag(name = "breadcrumb"))
 public abstract class AbstractBreadcrumb extends UIOutput {
-
 
     public static final String COMPONENT_FAMILY = "org.richfaces.bootstrap.Breadcrumb";
     public static final String COMPONENT_TYPE = "org.richfaces.bootstrap.Breadcrumb";
 
+    public static final String DIVIDER_DEFAULT = "/";
+
+    /**
+     * The divider is a character between each breadcrumb's component, default value is '/'
+     */
+    @Attribute(defaultValue = DIVIDER_DEFAULT, suggestedValue = DIVIDER_DEFAULT)
+    public abstract String getDivider();
 
 }
