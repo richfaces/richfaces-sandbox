7c37dbacd9b3d97475c180c4b06512196788e387
Lukas Macko <llukas3@gmail.com>
2013-07-23 12:05:06 +0200

    charts init

---
+++ charts/ui/src/main/java/org/richfaces/sandbox/chart/ChartRendererBase.java
@@ -0,0 +1,22 @@
+package org.richfaces.sandbox.chart;
+
+
+import org.richfaces.log.Logger;
+import org.richfaces.log.RichfacesLogger;
+import org.richfaces.renderkit.RendererBase;
+
+
+
+/**
+ *
+ *
+ * @author Lukas Macko
+ */
+public abstract class ChartRendererBase extends RendererBase{
+
+    public static final String RENDERER_TYPE = "org.richfaces.sandbox.ChartRenderer";
+
+    private static final Logger LOGGER = RichfacesLogger.RENDERKIT.getLogger();
+
+   
+}
