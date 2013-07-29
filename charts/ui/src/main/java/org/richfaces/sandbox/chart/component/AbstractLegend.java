1a2948816e40e92c56700c7406804b2368e779f5
Lukas Macko <llukas3@gmail.com>
2013-07-29 13:33:09 +0200

    Legend options

--- charts/ui/src/main/java/org/richfaces/sandbox/chart/component/AbstractLegend.java
+++ charts/ui/src/main/java/org/richfaces/sandbox/chart/component/AbstractLegend.java
@@ -16,11 +16,40 @@ import org.richfaces.cdk.annotations.Tag;
     tag = @Tag(name = "legend"))
 public abstract class AbstractLegend extends UIComponentBase{
 
+    /**
+     * Chart legend position
+     * allowed values: 
+     * <ul>
+     * <li>nw - top left </li>
+     * <li>sw - bottom left</li>
+     * <li>ne - top right</li>
+     * <li>se - bottom right</li>
+     * </ul>
+     * 
+     */
     @Attribute
-    public abstract String getPosition();
+    public abstract PositionType getPosition();
     
+    
+    /**
+     * The attribute defines the order of series labels in legend.
+     * If not specified the order labels is the same as the order of 
+     * series in facelet.
+     * Allowed values:
+     * <ul>
+     * <li>ascending</li>
+     * <li>descending</li>
+     * </ul>
+     * 
+     */
     @Attribute
-    public abstract String getSorting();
+    public abstract SortingType getSorting();
     
+    public enum PositionType{
+      nw,sw,ne,se  
+    };
     
+    public enum SortingType{
+      ascending,descending  
+    }
 }
