a62fc945807c07d729868d08eee2c390073b5386
Lukas Macko <llukas3@gmail.com>
2013-07-31 12:42:57 +0200

    Client-side event handling plotclick

--- charts/ui/src/main/resources/META-INF/resources/richfaces.charts.js
+++ charts/ui/src/main/resources/META-INF/resources/richfaces.charts.js
@@ -2,31 +2,75 @@
 
     rf.ui = rf.ui || {};
 
-    rf.ui.chart = function (id, data, options) {
+    rf.ui.chart = function (id, data, options,eventHandlers) {
         var mergedOptions = $.extend({}, defaultOptions, options);
+
+        
+        this.init(eventHandlers);
+        
         $super.constructor.call(this, id, mergedOptions);
         
-        
-        var plot=$.plot("#"+id,data,mergedOptions);
+        this.plot = $.plot("#"+id,data,mergedOptions);
         
         this.namespace = this.namespace || "." + rf.Event.createNamespace(this.name, this.id);
         this.attachToDom();
     };
 
-    rf.BaseComponent.extend(rf.ui.chart);
+    rf.ui.Base.extend(rf.ui.chart);
     var $super = rf.ui.chart.$super;
 
     var defaultOptions = {
-        someOption:'devaultValue'
+        grid:{clickable:true}
     };
 
-    $.extend(rf.ui.chart.prototype, (function () {
-
-        return {
-            name:"chart"
+    $.extend(rf.ui.chart.prototype, //(function () {
+        /*return*/{
+            name:"chart",
+            
+            init: function(eventHandlers){
+                this.eventHandlers = eventHandlers;
+            },
+        
+            __events:{
+                'onplotclick':'plotclick'
+            },     
+            __bindEventHandlers:function(){
+                for(e in this.__events){            //loop events handled
+                    if(this.eventHandlers[e]){      //is there handler for this ev
+                        $('#'+this.id).bind(
+                                this.__events[e],   //event name
+                                this.__getHandlerFunction(
+                                        this.id,
+                                        this.eventHandlers,
+                                        e));
+                    }
+                }
+                
+                
+        
+            },
+            __getHandlerFunction: function(id,handlers,eventName){
+                if(eventName==='onplotclick'){
+                    return function(event,pos,item){
+                        if(item !== null){
+                            //point in a chart clicked
+                            event.data={
+                                seriesIndex: item.seriesIndex,
+                                dataIndex:   item.dataIndex,
+                                x: item.datapoint[0],
+                                y: item.datapoint[1],
+                                item:item
+                            };
+                            handlers[eventName].call($('#'+id),event);
+                        }
+                    };
+                }
+            }        
+            
 
             // place shared prototype attributes and methods here.  Prepend "__" ahead of private methods
-        };
-    })());
+        }//;
+    //})()
+    );
 
 })(jQuery, window.RichFaces);
