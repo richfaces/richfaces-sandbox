c1a15ebabd4586082503c28da7462e7d901a29cb
Lukas Macko <llukas3@gmail.com>
2013-08-16 16:16:40 +0200

    _setOption handler

--- charts/ui/src/main/resources/META-INF/resources/richchart.js
+++ charts/ui/src/main/resources/META-INF/resources/richchart.js
@@ -117,23 +117,21 @@
         },
         // Use the _setOption method to respond to changes to options
         _setOption: function( key, value ) {
-            var redraw=true;
-            switch( key ) {
-                case "zoom":
-                    redraw=false;
-                    break;
-                case "handlers":
-                    redraw=false;
-                    break;
-                case "particularSeriesHandlers":
-                    redraw=false;
-                    break;
-            }
-
             // In jQuery UI 1.8, you have to manually invoke the _setOption method from the base widget
             $.Widget.prototype._setOption.apply( this, arguments );
             // In jQuery UI 1.9 and above, you use the _super method instead
             this._super( "_setOption", key, value );
+            
+            var redraw=true;
+            switch( key ) {
+                case "zoom":
+                case "handlers":
+                case "particularSeriesHandlers":
+                    this._unbind();
+                    this._registerListeners();
+                    redraw=false;
+                    break;
+            }
 
             if(redraw){
                 this._draw();
@@ -241,14 +239,18 @@
              //redraw chart without axis ranges
             this._draw();
         },
-
-        // Use the destroy method to clean up any modifications your widget has made to the DOM
-        destroy: function() {
-            this.plot.shutDown();
+                
+        _unbind:function(){
             this.element.unbind("plotclick");
             this.element.unbind("plothover");
             this.element.unbind("plotselected");
             this.element.unbind("mouseout");
+        },
+
+        // Use the destroy method to clean up any modifications your widget has made to the DOM
+        destroy: function() {
+            this.plot.shutDown();
+            this._unbind();
             // In jQuery UI 1.8, you must invoke the destroy method from the base widget
             $.Widget.prototype.destroy.call( this );
             // In jQuery UI 1.9 and above, you would define _destroy instead of destroy and not call the base method
