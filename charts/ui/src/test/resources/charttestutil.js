c079e8c1090d54767128ce32a794f120fbc3015a
Lukas Macko <llukas3@gmail.com>
2013-09-03 15:41:34 +0200

    charttestutil refactor pom.xml clean up

--- charts/ui/src/test/resources/charttestutil.js
+++ charts/ui/src/test/resources/charttestutil.js
@@ -1,34 +1,38 @@
-window.charttestutil = {};
+window.charttestutil = {
 
-window.charttestutil.hello = function() {
+   hello : function() {
 	  return "Hello World!";
-}
+   },
 
-window.charttestutil.seriesLength = function(id){
+   seriesLength : function(id){
 	  return $(document.getElementById(id)).chart("option","data").length;
-}
+   },
 
-window.charttestutil.dataLength = function(id,seriesIndex){
+   dataLength : function(id,seriesIndex){
       return $(document.getElementById(id)).chart("option","data")[seriesIndex].length;
-}
+   },
 
-window.charttestutil.pointX = function(id,seriesIndex,pointIndex){
+   pointX : function(id,seriesIndex,pointIndex){
 	  return $(document.getElementById(id)).chart("option","data")[seriesIndex].data[pointIndex][0];
-}
+   },
 
-window.charttestutil.pointY = function(id,seriesIndex,pointIndex){
+   pointY : function(id,seriesIndex,pointIndex){
 	  return $(document.getElementById(id)).chart("option","data")[seriesIndex].data[pointIndex][1];
-}
-window.charttestutil.pointXPos = function(id,seriesIndex,pointIndex){
+   },
+   
+   pointXPos : function(id,seriesIndex,pointIndex){
 	  //line chart only for now
 	  var xVal = window.charttestutil.pointX(id,seriesIndex,pointIndex);
 	  var plotObj = $(document.getElementById(id)).chart("getPlotObject");
 	  return plotObj.getXAxes()[0].p2c(xVal)+plotObj.offset().left;
-}
-window.charttestutil.pointYPos = function(id,seriesIndex,pointIndex){
+   },
+   
+   pointYPos : function(id,seriesIndex,pointIndex){
 	  //line chart only for now
 	  var yVal = window.charttestutil.pointY(id,seriesIndex,pointIndex);
 	  var plotObj = $(document.getElementById(id)).chart("getPlotObject");
 	  return plotObj.getYAxes()[0].p2c(yVal)+plotObj.offset().top-10;
+   }
+
 }
 
