61478aec45294ecb53f5d4809725a5ab7d420fef
Brian Leathem <bleathem@gmail.com>
2013-09-17 19:43:14 -0700

    component-template copy to orderingList

---
+++ orderingList/ui/src/main/java/org/richfaces/renderkit/ComponentNameRendererBase.java
@@ -0,0 +1,16 @@
+package org.richfaces.renderkit;
+
+import org.richfaces.component.ComponentName;
+
+import javax.faces.application.ResourceDependencies;
+import javax.faces.application.ResourceDependency;
+import javax.faces.component.UIComponent;
+
+@ResourceDependencies({
+        @ResourceDependency(library = "org.richfaces", name = "ajax.reslib"),
+        @ResourceDependency(library = "org.richfaces", name = "base-component.reslib"),
+        @ResourceDependency(library = "org.richfaces", name = "componentName.js"),
+        @ResourceDependency(library = "org.richfaces", name = "componentName.css")})
+public abstract class ComponentNameRendererBase extends RendererBase {
+    public static final String RENDERER_TYPE = "org.richfaces.ComponentNameRenderer";
+}
