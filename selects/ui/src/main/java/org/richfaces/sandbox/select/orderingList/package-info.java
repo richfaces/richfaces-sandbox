21cea85500d234b6fd2d86f40cc04d4af6c24533
Brian Leathem <bleathem@gmail.com>
2013-10-17 14:19:27 -0700

    Removed the selects components, as they have been moved into the framework

--- selects/ui/src/main/java/org/richfaces/sandbox/select/orderingList/package-info.java
+++
@@ -1,6 +0,0 @@
-@TagLibrary(shortName = "sandbox", prefix = "s", uri = "http://richfaces.org/sandbox/prototyping",
-        displayName="X")
-package org.richfaces.sandbox.select.orderingList;
-
-import org.richfaces.cdk.annotations.TagLibrary;
-
--- selects/ui/src/main/java/org/richfaces/sandbox/select/pickList/package-info.java
+++
@@ -1,6 +0,0 @@
-@TagLibrary(shortName = "sandbox", prefix = "s", uri = "http://richfaces.org/sandbox/prototyping",
-        displayName="X")
-package org.richfaces.sandbox.select.pickList;
-
-import org.richfaces.cdk.annotations.TagLibrary;
-
