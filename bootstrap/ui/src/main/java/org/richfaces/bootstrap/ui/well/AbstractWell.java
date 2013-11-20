9b62ac29991b88f26e490146ba5d3ae3da8edbe2
Lukas Eichler <LukasEichler1@gmail.com>
2013-11-20 18:21:25 +0100

    RFSBOX-110 Well Implementation

--- bootstrap/ui/src/main/java/org/richfaces/bootstrap/ui/well/AbstractWell.java
+++ bootstrap/ui/src/main/java/org/richfaces/bootstrap/ui/well/AbstractWell.java
@@ -1,25 +1,44 @@
 package org.richfaces.bootstrap.ui.well;
 
-import org.richfaces.bootstrap.ui.navitem.NavItemRendererBase;
-import org.richfaces.cdk.annotations.JsfComponent;
-import org.richfaces.cdk.annotations.JsfRenderer;
-import org.richfaces.cdk.annotations.Tag;
+import org.richfaces.cdk.annotations.*;
 import org.richfaces.ui.attribute.CoreProps;
 
 import javax.faces.component.UIOutput;
 
 /**
- * base class for navItem component
+ * base class for Well component
  *
  * @author Lukas Eichler
  */
 @JsfComponent(
         type = AbstractWell.COMPONENT_TYPE,
         family = AbstractWell.COMPONENT_FAMILY,
-        renderer = @JsfRenderer(type = NavItemRendererBase.RENDERER_TYPE),
-        tag = @Tag(name = "navItem"))
+        renderer = @JsfRenderer(type = WellRendererBase.RENDERER_TYPE),
+        tag = @Tag(name = "well"))
 public abstract class AbstractWell extends UIOutput implements CoreProps {
     public static final String COMPONENT_FAMILY = "org.richfaces.bootstrap.Well";
     public static final String COMPONENT_TYPE = "org.richfaces.bootstrap.Well";
 
+    @Attribute(suggestedValue = "large" + ","
+            + "small", description = @Description("Css based predefined sizes"))
+    public abstract String getSize();
+
+    public String generateClasses() {
+        StringBuilder sb = new StringBuilder("well");
+
+        String size = getSize();
+        if (size != null && !size.isEmpty()) {
+            if (size.equals("large")) {
+                sb.append(" well-large");
+            } else if (size.equals("small")) {
+                sb.append(" well-small");
+            }
+        }
+
+        if (getStyleClass() != null) {
+            sb.append(" ").append(getStyleClass());
+        }
+
+        return sb.toString();
+    }
 }
